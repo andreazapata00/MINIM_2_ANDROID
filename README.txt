ac3306ef68709a1c70d2983ee7ed8a5705951902
andreazapata00 <114937465+andreazapata00@users.noreply.github.com>
2024-05-31 17:28:31 +0200

    v0

---
+++ README.txt
@@ -0,0 +1,7 @@
+Segona versió:
+- Login i registre funcionen correctament.
+- S'ha implementat una Progress Bar
+- S'ha implementat una Splash screen
+-Un usuari que ha iniciat sessió a un dispositiu no cal que torni a escriure les seves credencials
+
+
---
+++ app/src/main/java/com/example/client_dsa/API.java
@@ -0,0 +1,27 @@
+package com.example.client_dsa;
+
+import com.example.client_dsa.Classes.*;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+public interface API {
+    //Un usuari es registra
+    @POST("usuaris/registreUsuari")
+    Call<RegisterComp> registre(@Body RegisterComp register);
+
+    //Un usuari inicia sessió
+    @POST("usuaris/login")
+    Call<LoginComp> login(@Body LoginComp login);
+
+    //Retorna una llista amb els objectes de la botiga
+    @GET("items/llista")
+    Call<Item> getItems(@Body Item llistaItem);
+
+    //FALTA @POST per quan compres un item
+
+    //Per auntenticar l'usuari
+    // @GET("/user")
+    //Call<Usuari> getUserDetails(@Header("Authorization") String credentials);
+}
